c9912ea73e06e53b82b72010b6bf26a8a7f81bf8
FelixDaDalt <felixdadalt@gmail.com>
2022-05-05 15:44:02 -0300

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCamp_Challenge_4
+{
+    public class Persona
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"El nombre es: {this.Name}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCamp_Challenge_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicio1();
+            Console.ReadKey();
+        }
+
+        static void Ejercicio1()
+        {
+            Persona[] per = new Persona[3];
+            for (int x = 0; x < per.Length; x++)
+            {
+                Console.WriteLine("Nombre: ");
+                per[x] = new Persona();
+                per[x].Name = Console.ReadLine();
+            }
+            for (int x = 0; x < per.Length; x++)
+            {
+                Console.WriteLine(per[x].ToString());
+            }
+            
+        }
+    }
+}
